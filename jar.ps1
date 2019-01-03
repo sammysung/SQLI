bd2db8679d9e35815132829c078a00f088fbd6f6
Sam <samwoodthirteen@gmail.com>
2019-01-03 13:04:05 -0600

    Small script fixes

--- jar.ps1
+++ jar.ps1
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream
 # Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 # This version should be used on Windows and Linux, and possibly OSX 10.12 and up.
 
+=======
+#Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+#!/usr/bin/pwsh
+>>>>>>> Stashed changes
 javac -d . *.java
 jar -cvfm frame.jar manifest.txt com/sqli/framework/*.class
 java -jar frame.jar -hq
