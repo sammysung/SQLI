1a1edeca5a9936063695d76d03cce7fb52b3be19
Sam <samwoodthirteen@gmail.com>
2019-01-06 21:30:53 -0600

    Setup script to install Windows dev tools
    Run choco.ps1 script, it'll ask you for admin rights and setup Chocolatey, which will then run winInstall.ps1 to install the tools. winInstall.bat can be used to reinstall the tools, if needed.

---
+++ winInstall.bat
@@ -0,0 +1,6 @@
+:: Only really useful after running the chocolatey installer in Powershell
+
+choco install notepadplusplus -y
+choco install jdk11 -y
+choco install ruby -y
+choco install intellijidea-community -y
---
+++ winInstall.ps1
@@ -0,0 +1,10 @@
+# Read this before running! If it'll even let you right off..
+# This will install notepad++, jdk11, ruby, and the community edition of intellj; this happens after chocolatey itself is installed.
+# The way chocolatey works, it seems it will reinstall them if you did this through other means, but it will also setup the path for you correctly.
+# Must be ran as Admin to work properly!
+# This really should only need to be ran once, but let me know if something goes wrong.
+
+choco install notepadplusplus -y
+choco install jdk11 -y
+choco install ruby -y
+choco install intellijidea-community -y
