9c6e777dc0b02c8d809b061970340f26d912cb14
Sam <samwoodthirteen@gmail.com>
2019-01-19 16:43:31 -0600

    Updated linux install script to be more concise

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install code
-
 sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev ruby-all-dev default-jre snapd
 sudo snap install powershell --classic
+sudo snap install vscode --classic
