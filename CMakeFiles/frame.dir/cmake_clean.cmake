eb44e0f0a105fe93dfa35cb9e7638e37b8e44246
Sam <samwoodthirteen@gmail.com>
2019-01-19 14:44:05 -0600

    Got rid of temp files we don't need.

--- CMakeFiles/frame.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/frame"
-  "frame.jar"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/frame.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dude/Documents/git/SQLI
-# Build directory: /Users/dude/Documents/git/SQLI
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
