97ec1e1386c4ae4aa0557d64bfe89ceb776fef3a
Sam <samwoodthirteen@gmail.com>
2019-01-28 16:49:00 -0600

    Added make install function on Windows
    The other platforms have it too, I just haven't fixed it yet.

--- scripts/WinMake/makeM.ps1
+++ scripts/WinMake/makeM.ps1
@@ -1,6 +1,9 @@
 cd ..
 cd ..
 cmake . -S . -G "MinGW Makefiles" -B ./mingw
+cp scripts/pwsh/help.ps1 mingw/help.ps1
 cd mingw
-mingw32-make.exe
 mingw32-make.exe install
+mingw32-make.exe
+cd ..
+cd scripts/WinMake
